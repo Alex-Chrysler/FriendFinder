e49935009cbfe2944cdd6405ee95693313365d34
achrysler <achrysler36@gmail.com>
2018-10-25 13:38:59 -0500

    couple more small changed

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,37 @@
-var friendsArray = []
+var friendsArray = [
+    {
+        "answers": [
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5"
+        ],
+        "name": "Dylan",
+        "picture": "https://media.newyorker.com/photos/5b2e5a90a721442d1d96d47c/4:3/w_960,c_limit/Petrusich-Bob-Dylan-Whiskey_01.jpg"
+        },
+        {
+            "answers": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+            ],
+            "name": "Don",
+            "picture": "http://theshowbizlife.com/wp-content/uploads/2018/02/don-film.jpg"
+            },
+]
 
 
 module.exports = friendsArray
