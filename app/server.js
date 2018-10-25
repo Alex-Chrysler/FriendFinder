551bcbb367a51829b93bacb1564cac510f5a6446
achrysler <achrysler36@gmail.com>
2018-10-25 13:14:41 -0500

    added a slash

--- app/server.js
+++ app/server.js
@@ -16,7 +16,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static("public"))
+app.use(express.static("/public"))
 
 // ================================================================================
 // ROUTER
