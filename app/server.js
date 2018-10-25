b4e78265bb5633b41a6f810a03d33f0d38edb4b4
achrysler <achrysler36@gmail.com>
2018-10-25 12:54:09 -0500

    fixexd port part 2

--- app/server.js
+++ app/server.js
@@ -31,7 +31,7 @@ require("./routing/htmlRoutes")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("App listening on PORT " + PORT);
   });
 
