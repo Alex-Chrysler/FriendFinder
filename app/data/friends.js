a79e9fb5abc55766ec32f97d8cccd429d7e57565
achrysler <achrysler36@gmail.com>
2018-10-25 21:37:30 -0500

    Working?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,37 @@
 var friendsArray = [
     {
         "answers": [
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5",
-        "5"
-        ],
-        "name": "Dylan",
-        "picture": "https://media.newyorker.com/photos/5b2e5a90a721442d1d96d47c/4:3/w_960,c_limit/Petrusich-Bob-Dylan-Whiskey_01.jpg"
-        },
-        {
+            "4",
+            "1",
+            "5",
+            "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "3",
+            "1"
+            ],
+            "name": "Spongebob Squarepants",
+            "picture": "https://vignette.wikia.nocookie.net/king-harkinian/images/4/47/Spongebob.png/revision/latest?cb=20170518185222"
+            },
+            {
+            "answers": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+            ],
+            "name": "Jesus",
+            "picture": "https://i.kym-cdn.com/entries/icons/facebook/000/009/556/jesus-bleu-mauve.jpg"
+            },
+            {
             "answers": [
             "1",
             "1",
@@ -28,9 +44,25 @@ var friendsArray = [
             "1",
             "1"
             ],
-            "name": "Don",
-            "picture": "http://theshowbizlife.com/wp-content/uploads/2018/02/don-film.jpg"
+            "name": "Batman",
+            "picture": "https://thenypost.files.wordpress.com/2018/09/180912-ben-affleck-batman-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1"
             },
+            {
+                "answers": [
+                "1",
+                "4",
+                "5",
+                "5",
+                "2",
+                "2",
+                "4",
+                "4",
+                "2",
+                "5"
+                ],
+                "name": "John Cena",
+                "picture": "https://wrestleyard.com/wp-content/uploads/2018/10/John-Cena-WWE-SSD.jpg"
+                },
 ]
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,76 +1,32 @@
-
-let friendsData = require("../data/friends");
+let friendsArray = require("../data/friends");
 
 module.exports = function(app) {
 
-
-
     app.get("/api/friends", function(req, res) {
-        return res.json(friendsData);
+        return res.json(friendsArray);
     });
 
-/*app.post("/api/firends", function(req, res) {
 
-    var user = req.body;
-
-    for(var i = 0; i<user.answers.length; i++){
-        user.answers[i] = parseInt(user.answers[i])
-    }
-
-    var bestFriendIndex = 0;
-    var minimumDifference = 40;
-
-    for (var i = 0; i < friendsData.length; i++) {
-        var totalDifference = 0;
-        for(var j = 0; j < friendsData[i].answers.length; j++){
-            var difference = Math.abs(user.answers[j] - friendsData[i].answers[j]);
-            totalDifference += difference;
-        }
-        if(totalDifference < minimumDifference) {
-            bestFriendIndex = i;
-            minimumDifference = totalDifference;
-    }
-})*/
 
    app.post("/api/friends", function(req, res) {
         var newFriend = req.body
        
         var totalDifference = 1000;
 
-        for(let i in friendsData){
-
-           // console.log('friend = ' + JSON.stringify(friendsData[i]));
-
-
+        for(let i in friendsArray){
             var diff = 0
-            for (let j in friendsData[i].scores){
-                 diff += Math.abs(parseInt(friendsData[i].answers[j]) - parseInt(req.body.answers[j]));
-
-                    /*console.log("other thing" + friendsData[i].answers[j])
-                    console.log("user response" + userResponses)
-                     console.log('diff = ' + diff)*/
+            for (let j in friendsArray[i].answers){
+                 diff += Math.abs(parseInt(friendsArray[i].answers[j])-parseInt(req.body.answers[j]));
         }
   
         if (diff < totalDifference) {
-      /*console.log('Closest match found = ' + diff);
-      console.log('Friend name = ' + friendsData[i].name);
-      console.log('Friend image = ' + friendsData[i].picture);*/
-      console.log(diff)
-      console.log(totalDifference)
-
-        totalDifference = diff
-        console.log(diff)
-
-      var dataToSend = {name:friendsData[i].name, image: friendsData[i].picture} ;
-    
-
-      console.log(dataToSend)
-    
-}
+            totalDifference = diff
+            var dataToSend = {name:friendsArray[i].name, image: friendsArray[i].picture} ;    
+        }
 }
 
 //console.log(newFriend)
-friendsData.push(newFriend);
+friendsArray.push(newFriend);
 res.send(dataToSend)
 
     })
